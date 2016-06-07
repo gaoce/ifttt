02909a0dd12ccaa8df7db7cccc2d0ae8cdb7a77d
gaoce <gaoce@coe.neu.edu>
2016-06-07 17:44:34 -0400

    Add command line interface

--- ifttt/__init__.py
+++ ifttt/__init__.py
@@ -3,6 +3,7 @@ from ifttt.email import email_feeds
 from ifttt.feeds import *
 import os.path
 import logging
+import argparse
 
 
 home_dir = os.path.expanduser('~')
@@ -12,9 +13,23 @@ logging.basicConfig(filename=os.path.join(home_dir, '.ifttt/log'),
                     level=logging.INFO)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='ifttt')
+    parser.add_argument('-no-email', action='store_true', default=False,
+                        help='Do not send emails [default: False]')
+
+    return parser.parse_args()
+
+
 def main():
     logging.info('Start ifttt session')
+    args = parse_args()
+
     file_path = os.path.join(home_dir, '.ifttt/rss.opml')
     rss_feeds = update_feeds(file_path)
-    email_feeds(rss_feeds)
+
+    # Opt-out email sending
+    if not args.no_email:
+        email_feeds(rss_feeds)
+
     logging.info('Finish ifttt session')
